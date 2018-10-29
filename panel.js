8bfad531bc68d142574f233aa1b1e57cb02f18be
mkuegeler <mkuegeler@mac.com>
2018-10-29 18:52:37 +0100

    initial application files added

---
+++ panel.js
@@ -0,0 +1,20 @@
+// panel config file
+const config = {
+    icons: {
+        on: 'on.svg',
+        off: 'off.svg'
+    },
+    itemList: [{
+            id: 0,
+            productNo: 'P0001'
+        },
+        {
+            id: 1,
+            productNo: 'P0001'
+        },
+        {
+            id: 2,
+            productNo: 'P0001'
+        }
+    ]
+};
